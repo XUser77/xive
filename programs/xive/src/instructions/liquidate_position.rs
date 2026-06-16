65124471a5d73f8f22a49ca221686f42f458c028
Timur <xuser77@gmail.com>
2026-06-16 11:58:44 +0500

    Xive liquidate position collateral transfer

--- programs/xive/src/instructions/liquidate_position.rs
+++ programs/xive/src/instructions/liquidate_position.rs
@@ -1,10 +1,12 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::Mint;
-use crate::{Position, POSITION_SEED, PRICE_TIMEOUT, COLLATERAL_SEED, XIVE_SEED, VAULT_ADDRESS };
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token;
+use anchor_spl::token::{Mint, Token, TokenAccount, TransferChecked};
+use crate::{Position, POSITION_SEED, PRICE_TIMEOUT, COLLATERAL_SEED, XIVE_SEED };
 use crate::errors::XiveError;
 use crate::state::collateral::Collateral;
 use crate::state::xive::Xive;
-use crate::utils::get_position_bps;
+use crate::utils::{get_position_bps, get_vault_pda_address};
 
 #[derive(Accounts)]
 pub struct LiquidatePosition<'info> {
@@ -33,16 +35,35 @@ pub struct LiquidatePosition<'info> {
 
     #[account(
         mut,
+        associated_token::mint = collateral_mint,
+        associated_token::authority = xive,
+    )]
+    pub xive_collateral_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        associated_token::mint = collateral_mint,
+        associated_token::authority = signer,
+    )]
+    pub vault_collateral_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
         seeds = [XIVE_SEED.as_bytes()],
         bump = xive.bump,
     )]
     pub xive: Account<'info, Xive>,
 
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+
 }
 
 pub fn liquidate_position(ctx: Context<LiquidatePosition>) -> Result<()> {
 
-    require!(ctx.accounts.signer.key() == VAULT_ADDRESS, XiveError::SignerNotVault);
+    require!(ctx.accounts.signer.key() == get_vault_pda_address()?, XiveError::SignerNotVault);
 
     let position = &mut ctx.accounts.position;
     let collateral = &ctx.accounts.collateral;
@@ -66,14 +87,28 @@ pub fn liquidate_position(ctx: Context<LiquidatePosition>) -> Result<()> {
 
     require!(bps >= collateral.liquidation_ltv, XiveError::HealthyPosition);
 
+    let collateral_amount = position.collateral_amount;
+
     xive.vault_balance = xive.vault_balance
         .checked_sub(i64::try_from(position.loan_amount).map_err(|_| XiveError::MathOverflow)?)
         .ok_or(XiveError::MathOverflow)?;
     position.loan_amount = 0;
     position.close_date = now;
-
     position.collateral_amount = 0;
-    // TODO: Transfer collateral to vault
+
+    token::transfer_checked(
+        CpiContext::new(
+            ctx.accounts.token_program.key(),
+            TransferChecked {
+                mint: ctx.accounts.collateral_mint.to_account_info(),
+                from: ctx.accounts.xive_collateral_ata.to_account_info(),
+                to: ctx.accounts.vault_collateral_ata.to_account_info(),
+                authority: ctx.accounts.xive.to_account_info(),
+            }
+        ),
+        collateral_amount,
+        ctx.accounts.collateral_mint.decimals,
+    )?;
 
     Ok(())
 }
